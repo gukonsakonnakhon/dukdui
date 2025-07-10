6ed70bc7a5c25a9b359b6470f0192ca407d30331
Sanli Saeti <azukiairi777@gmail.com>
2025-07-10 11:37:34 +0700

    Create deposit.lua

---
+++ deposit.lua
@@ -0,0 +1,14 @@
+return function(amount)
+    local result = nil
+    local completed = false
+    
+    task.spawn(function()
+        local repStorage = game:GetService("ReplicatedStorage")
+        local netThing = require(repStorage.Modules.Core.Net)
+        result = netThing.get("transfer_funds", "hand", "bank", amount)
+        completed = true
+    end)
+    
+    repeat task.wait() until completed
+    return result
+end
